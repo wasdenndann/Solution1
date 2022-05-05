5408b73a2efba66f927d605829a40614e3610491
Peter Pfrang <wasdenndann@peterspoint.de>
2022-05-05 06:43:31 +0200

    1st time

---
+++ Solution1/Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Circle
+    {
+        private int _Radius;    // property
+        public int Radius  // property
+        {
+            get { return _Radius;}
+            set {
+                if (value >= 0)
+                    _Radius = value;
+                else Console.WriteLine("unzulässiger negativer Wert.");}
+        }
+        private double _Xcoordinate;    // property
+        public double Xcoordinate
+        {
+            get { return _Xcoordinate; }
+            set { _Xcoordinate = value; }
+        }
+        private double _Ycoordinate;    // property
+        public double Ycoordinate 
+        { 
+            get { return _Ycoordinate; }
+            set { _Ycoordinate = value; }
+        }
+        public double GetArea()
+        {
+            return Math.Pow(Radius, 2) * Math.PI;
+        }
+        public double GetCircumference()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public void Move(double dx, double dy)
+        {
+            Xcoordinate += dx;
+            Ycoordinate += dy;
+        }
+        public void Move(double dx, double dy, int dRadius)
+        {
+            Move(dx, dy);
+            Radius += dRadius;
+        }
+        public int Bigger(Circle kreis)
+        {
+            if (kreis == null || Radius > kreis.Radius) return 1;
+            if (Radius < kreis.Radius) return -1;
+            return 0;
+        }
+    }
+}
---
+++ Solution1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Circle kreis1 = new Circle();
+            Circle kreis2 = kreis1;
+            kreis1.Radius = 10;
+
+            Console.WriteLine(kreis2.Radius);
+
+            kreis2.Radius = 20;
+            Console.WriteLine(kreis2.Radius);
+            Console.WriteLine(kreis1.Radius);
+            Console.WriteLine();
+
+            long value1 = 64;
+            long value2 = value1;
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            value1 = 100;
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+
+            Circle kreis3 = new Circle {Radius = 5};
+            //kreis3.Radius = 5;
+
+
+            Console.WriteLine("kreis1, Fläche {0}, Umfang {1}", kreis1.GetArea(), kreis1.GetCircumference());
+            Console.WriteLine("kreis2, Fläche {0}, Umfang {1}", kreis2.GetArea(), kreis2.GetCircumference());
+            Console.WriteLine("kreis3, Fläche {0}, Umfang {1}", kreis3.GetArea(), kreis3.GetCircumference());
+
+            kreis1.Xcoordinate = -100;
+            kreis1.Ycoordinate = 90;
+            kreis1.Move(120, -200);
+
+            Console.WriteLine();
+            
+            
+
+        }
+    }
+    class Demo
+    {
+        public void Change(Circle @object)
+        {
+            @object.Radius = 1;
+        }
+    }
+}
